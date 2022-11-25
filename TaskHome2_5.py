473057dbe9247d22d3c46ca2038c8b90789c97b8
Dr.Ddiezel93 <ghek93@mail.ru>
2022-11-26 00:39:39 +0300

    Добавил решение задачи 5 к семинару 2

---
+++ TaskHome2_5.py
@@ -0,0 +1,15 @@
+# 5 Реализуйте алгоритм перемешивания списка.
+
+# Алгоритм перетасовки Фишера–Йейтса
+
+import random
+
+def randomize (arr, n):
+    for i in range(n-1,0,-1):
+        j = random.randint(0,i+1)
+        arr[i],arr[j] = arr[j],arr[i]
+    return arr
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8]
+n = len(arr)
+print(randomize(arr, n))
