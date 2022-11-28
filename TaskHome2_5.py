92bf47e2527fb838849a1f88052e8c91ee149bdb
Dr.Ddiezel93 <ghek93@mail.ru>
2022-11-28 20:02:11 +0300

    Устранил замечания по задаче 5

--- TaskHome2_5.py
+++ TaskHome2_5.py
@@ -4,10 +4,10 @@
 
 import random
 
-def randomize (arr, n):
-    for i in range(n-1,0,-1):
-        j = random.randint(0,i+1)
-        arr[i],arr[j] = arr[j],arr[i]
+def randomize(arr, n):
+    for i in range(n-1, 0, -1):
+        j = random.randint(0, i+1)
+        arr[i], arr[j] = arr[j], arr[i]
     return arr
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8]
