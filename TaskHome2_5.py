e5294f1283a188548dd094fc69c79caf631fd685
Dr.Ddiezel93 <ghek93@mail.ru>
2022-11-28 20:12:16 +0300

    Оптимизировал решение задачи  5

--- TaskHome2_5.py
+++ TaskHome2_5.py
@@ -2,14 +2,13 @@
 
 # Алгоритм перетасовки Фишера–Йейтса
 
-import random
+from random import randint
 
-def randomize(arr, n):
-    for i in range(n-1, 0, -1):
-        j = random.randint(0, i+1)
+def randomize(arr):
+    for i in range(len(arr)):
+        j = randint(0, len(arr) - 1)
         arr[i], arr[j] = arr[j], arr[i]
-    return arr
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8]
-n = len(arr)
-print(randomize(arr, n))
+array = [1, 2, 3, 4, 5, 6, 7, 8]
+randomize(array)
+print(array)
