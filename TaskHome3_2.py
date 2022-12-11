0cd3fed106b7469bdb0ba0684e5c1fe7929328d6
Dr.Ddiezel93 <ghek93@mail.ru>
2022-12-11 12:23:08 +0300

    Оптимизировал решение задачи 2 к семинуру 3 через List comprehension

--- TaskHome3_2.py
+++ TaskHome3_2.py
@@ -8,11 +8,15 @@ import math
 lst_1 = [2, 3, 4, 5, 6]
 lst_2 = [2, 3, 5, 6]
 
-def MultiPairsNumb (lst):
-    multi_lst = []
-    for i in range(math.ceil(len(lst) / 2)):
-        multi_lst.append(lst[i] * lst[(-i) - 1])
-    print(f'{lst} => {multi_lst}')
+# def MultiPairsNumb (lst):
+#     multi_lst = []
+#     for i in range(math.ceil(len(lst) / 2)):
+#         multi_lst.append(lst[i] * lst[(-i) - 1])
+#     print(f'{lst} => {multi_lst}')
 
-MultiPairsNumb(lst_1)
-MultiPairsNumb(lst_2)
+multi_lst_1 = [lst_1[i] * lst_1[(-i) - 1] for i in range(math.ceil(len(lst_1) / 2))]
+multi_lst_2 = [lst_2[i] * lst_2[(-i) - 1] for i in range(math.ceil(len(lst_2) / 2))]
+
+
+print(multi_lst_1)
+print(multi_lst_2)
