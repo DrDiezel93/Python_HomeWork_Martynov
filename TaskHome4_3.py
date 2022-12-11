f7ddf7dcb49737b3c8b2568984e630b68ab43f25
Dr.Ddiezel93 <ghek93@mail.ru>
2022-12-11 12:02:06 +0300

    Оптимизировал решение задачи 3 семинару 4 через filter и lambda

--- TaskHome4_3.py
+++ TaskHome4_3.py
@@ -3,10 +3,12 @@
 # - при [1, 1, 2, 3, 3, 4, 1, 5, 7, 8, 8, 7, 9]     ->        [2, 4, 5, 9]
 
 lst_1 = [1, 1, 2, 3, 3, 4, 1, 5, 7, 8, 8, 7, 9]
-lst_2 = []
 
-for item in lst_1:
-    if lst_1.count(item) == 1:
-        lst_2.append(item)
+# lst_2 = []
+# for item in lst_1:
+#     if lst_1.count(item) == 1:
+#         lst_2.append(item)
+
+lst_2 = list(filter(lambda x: lst_1.count(x) == 1, lst_1))
 
 print(f'при {lst_1} - > {lst_2}')
